d337a0e91f109a162688fd7a828c0e2c9651bab4
Nelfa08 <corentin.roddier@gmail.com>
2021-10-30 20:24:07 +0200

    ajout choix box

--- JS/app.js
+++ JS/app.js
@@ -15,12 +15,12 @@ $(document).ready(function(){
 
 function change(event) {
     if(event.id=="small-black") {
-        document.querySelector(".pro-img").src = '/img/smallBox/02_-_Black.png';
+        document.querySelector(".pro-img").src = 'img/smallBox/02_-_Black.png';
     } else if(event.id=="small-red") {
-        document.querySelector(".pro-img").src = '/img/smallBox/02_-_Red.png';
+        document.querySelector(".pro-img").src = 'img/smallBox/02_-_Red.png';
     } else if(event.id=="small-blue") {
-        document.querySelector(".pro-img").src = '/img/smallBox/02_-_Blue.png';
+        document.querySelector(".pro-img").src = 'img/smallBox/02_-_Blue.png';
     } else if(event.id=="small-yellow") {
-        document.querySelector(".pro-img").src = '/img/smallBox/02_-_Yellow.png';
+        document.querySelector(".pro-img").src = 'img/smallBox/02_-_Yellow.png';
     }
 }

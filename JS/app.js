f18b5fdbdeabd4f7be306f76cc97aaf91b8f6e9c
Nelfa08 <corentin.roddier@gmail.com>
2021-10-30 23:18:20 +0200

    ajout choix box

--- JS/app.js
+++ JS/app.js
@@ -13,14 +13,26 @@ $(document).ready(function(){
     });
 });
 
-function change(event) {
+function changesmall(event) {
     if(event.id=="small-black") {
-        document.querySelector(".pro-img").src = 'img/smallBox/02_-_Black.png';
+        document.querySelector(".pro-img-small").src = 'img/smallBox/02_-_Black.png';
     } else if(event.id=="small-red") {
-        document.querySelector(".pro-img").src = 'img/smallBox/02_-_Red.png';
+        document.querySelector(".pro-img-small").src = 'img/smallBox/02_-_Red.png';
     } else if(event.id=="small-blue") {
-        document.querySelector(".pro-img").src = 'img/smallBox/02_-_Blue.png';
+        document.querySelector(".pro-img-small").src = 'img/smallBox/02_-_Blue.png';
     } else if(event.id=="small-yellow") {
-        document.querySelector(".pro-img").src = 'img/smallBox/02_-_Yellow.png';
+        document.querySelector(".pro-img-small").src = 'img/smallBox/02_-_Yellow.png';
+    }
+}
+
+function changebig(event) {
+    if(event.id=="big-black") {
+        document.querySelector(".pro-img-big").src = 'img/bigBox/05_-_Black.png';
+    } else if(event.id=="big-red") {
+        document.querySelector(".pro-img-big").src = 'img/bigBox/05_-_Red.png';
+    } else if(event.id=="big-blue") {
+        document.querySelector(".pro-img-big").src = 'img/bigBox/05_-_Blue.png';
+    } else if(event.id=="big-yellow") {
+        document.querySelector(".pro-img-big").src = 'img/bigBox/05_-_Yellow.png';
     }
 }
